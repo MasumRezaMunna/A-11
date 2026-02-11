3de44f15d0eecd8b128fa2ee46906a6a6199ca6b
MasumRezaMunna <masumrezamunna2005@gmail.com>
2026-02-11 21:37:02 +0600

    updated axios file

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,25 +2,17 @@ import axios from "axios";
 import { auth } from "./firebase";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
-api.interceptors.request.use(
-  async (config) => {
-    const user = auth.currentUser || await new Promise(resolve => {
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            unsubscribe();
-            resolve(user);
-        });
-    });
-
-    if (user) {
-      const token = await user.getIdToken(true);
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
+api.interceptors.request.use(async (config) => {
+  const user = auth.currentUser;
+  
+  if (user) {
+    const token = await user.getIdToken();
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => Promise.reject(error));
 
 export default api;
